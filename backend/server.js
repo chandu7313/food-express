41a19b25716e96585b0250b909c1706e4d4185ad
Kavyasr <kavyasri28@gmail.com>
2026-04-21 21:29:05 +0530

    added dockefile

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.get('/', (req, res) => {
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
