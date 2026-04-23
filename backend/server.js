9e21e22a32f5d885f1f85c7c81680d08872cd30e
chandu7313 <mrcm7313@gmail.com>
2026-04-23 15:40:02 +0530

    cors updated

--- backend/server.js
+++ backend/server.js
@@ -9,14 +9,7 @@ connectDB();
 
 const app = express();
 
-app.use(cors({
-  origin: [
-    "http://localhost:5174",
-    "http://localhost:5173",
-    "https://food-express-silk.vercel.app"
-  ],
-  credentials: true
-}));
+app.use(cors());
 
 app.use(express.json());
 
